6f37a1553036e89ef1fe318057f68df0a49a1673
car <carmelogustavo@gmail.com>
2019-01-12 18:15:13 +0100

    feat: text input changes state by name

--- screen/InputCharge.js
+++ screen/InputCharge.js
@@ -4,19 +4,36 @@ import { StyleSheet, TextInput, View } from 'react-native';
 export default class InputChange extends Component {
   state = {
     payment: '',
-    company: '',
+    collector: '',
     amount: 0
+  };
+
+  handleChange = (text, name) => {
+    this.setState({ [name]: text });
+    console.log(this.state);
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={{ height: 40 }} placeholder="payment" />
-        <TextInput style={{ height: 40 }} placeholder="collector" />
+        <TextInput
+          style={{ height: 40 }}
+          value={this.state.payment}
+          placeholder="payment"
+          onChangeText={text => this.handleChange(text, 'payment')}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          value={this.state.collector}
+          placeholder="collector"
+          onChangeText={text => this.handleChange(text, 'collector')}
+        />
         <TextInput
           style={{ height: 40 }}
           keyboardType="numeric"
+          value={this.state.amout}
           placeholder="amount"
+          onChangeText={text => this.handleChange(text, 'amount')}
         />
       </View>
     );
