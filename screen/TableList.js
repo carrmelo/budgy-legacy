1355c9400d6bffb87baadffa27a523b2a6abdd96
car <carmelogustavo@gmail.com>
2019-01-14 14:40:25 +0100

    feat: render db on table list. formatAmount function

--- screen/TableList.js
+++ screen/TableList.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Alert, StyleSheet, Text, View, ScrollView } from 'react-native';
-
-const test = (
-  <Text onPress={() => Alert.alert('Pressed')}>I'm gonna be a table</Text>
-);
+import formatAmount from '../lib/formatAmount';
 
 export default class TableList extends Component {
   state = {
@@ -16,24 +13,28 @@ export default class TableList extends Component {
   componentDidMount() {
     fetch('http://192.168.0.156:3000/payments')
       .then(response => response.json())
-      .then(data => console.log(data) || this.setState({ payments: [...data] }))
+      .then(data => this.setState({ payments: [...data] }))
       .catch(error => console.log(error));
-    console.log(this.state);
   }
 
   componentDidUpdate() {
     console.log('hola', this.state);
   }
+  calculateTotal() {}
 
   render() {
+    const { payments } = this.state;
+    console.log('In', !payments.length === false);
     return (
       <ScrollView style={styles.container}>
         <ScrollView style={styles.content} maximumZoomScale="10">
-          {Array(10)
-            .fill(test)
-            .map((test, i) => (
-              <View key={i}>{test}</View>
-            ))}
+          {payments.map(payment => (
+            <View style={styles.payment} key={payment._id}>
+              <Text>{payment.name}</Text>
+              <Text>{payment.collector}</Text>
+              <Text>{formatAmount(payment.amount)}</Text>
+            </View>
+          ))}
         </ScrollView>
         <Text style={{ flex: 1 }}>Total</Text>
       </ScrollView>
@@ -49,5 +50,8 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 10,
+  },
+  payment: {
+    flexDirection: 'row',
   },
 });
